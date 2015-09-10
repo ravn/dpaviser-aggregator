bf44e8478b3f86c2dfbf7f843852bf0e3ccaf753
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2015-09-10 17:05:04 +0200

    Fixed tests for Windows.

--- dpaviser-metadata-integration-test/src/test/java/helper/JHoveCommandPipeTest.java
+++ dpaviser-metadata-integration-test/src/test/java/helper/JHoveCommandPipeTest.java
@@ -1,5 +1,6 @@
 package helper;
 
+import com.google.common.base.Charsets;
 import dk.statsbiblioteket.dpaviser.metadatachecker.helper.JHoveCommandPipe;
 import org.testng.annotations.Test;
 
@@ -8,6 +9,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,8 +38,13 @@ public class JHoveCommandPipeTest {
     @Test
     public void testIfJHoveOutputIsTheSameAsInTheTestdata() throws Exception {
 
-        String targetClassesPath = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        File thisMavenModuleDir = new File(targetClassesPath, "../..");
+        URL location = getClass().getProtectionDomain().getCodeSource().getLocation();
+        System.out.println(location);
+        URI targetClassesURI = location.toURI();
+        System.out.println(targetClassesURI);
+
+        File targetClassesDir = new File(targetClassesURI);
+        File thisMavenModuleDir = new File(targetClassesDir, "../..");
         File jhoveBinDir = new File(thisMavenModuleDir, "../jhove/jhove-apps/target/appassembler/bin/");
         String canonicalPath = jhoveBinDir.getCanonicalPath();
 
@@ -43,31 +52,35 @@ public class JHoveCommandPipeTest {
             throw new FileNotFoundException(canonicalPath);
         }
         System.out.println(canonicalPath); // FIXME: Remove when ready.
+        String userDirPath = System.getProperty("user.dir");
+        System.out.println(userDirPath);
 
+        // Note: JHove output localized dates.
         Map<String, String> environmentVariables = new HashMap<>();
-        environmentVariables.put("TZ", "UTC");  // IMPORTANT: JHove output localized dates.
+//        environmentVariables.put("TZ", "UTC"); // Tell Linux to use UTC
+// Changed launcher script instead.
 
         JHoveCommandPipe commandPipe = new JHoveCommandPipe(canonicalPath, environmentVariables);
         try (
                 InputStream actual = commandPipe.apply(getClass().getResourceAsStream(BMA20150831_X11_0002_PDF));
                 InputStream expected = getClass().getResourceAsStream(BMA20150831_X11_0002_XML)
         ) {
-            List<String> actualList = StringListForInputStream(actual);
+            List<String> actualList = StringListForInputStream(actual,Charset.defaultCharset());
             actualList.remove(5); // <lastModified>2015-09-03T17:09:01+02:00</lastModified>
             actualList.remove(3); // <repInfo uri="/tmp/JHoveCommandPipe6233676701247156809.tmp">
             actualList.remove(2); // <date>2015-09-03T17:09:01+02:00</date>
             actualList.remove(1); // <jhove xmlns:xsi="http: ... release="1.12.0-SNAPSHOT" date="2015-09-03">
 
-            List<String> expectedList = StringListForInputStream(expected);
+            List<String> expectedList = StringListForInputStream(expected, UTF_8);
             expectedList.remove(5);
             expectedList.remove(3);
             expectedList.remove(2);
             expectedList.remove(1);
-            assertEquals(join("\n", expectedList), join("\n", actualList), "JHove output does not match cached version");
+            assertEquals(join("\n", actualList), join("\n", expectedList), "JHove output does not match cached version");
         }
     }
 
-    private List<String> StringListForInputStream(InputStream actual) {
-        return new BufferedReader(new InputStreamReader(actual, UTF_8)).lines().collect(toList());
+    private List<String> StringListForInputStream(InputStream actual, Charset charset) {
+        return new BufferedReader(new InputStreamReader(actual, charset)).lines().collect(toList());
     }
 }
